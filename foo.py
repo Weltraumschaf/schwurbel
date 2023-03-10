a1f49d638116ecb44cce1735ed036ee11ba4f7e3
Sven Strittmatter <ich@weltraumschaf.de>
2023-03-10 21:23:00 +0100

    Refactoring
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- foo.py
+++ foo.py
@@ -1,15 +1,44 @@
 import xml.etree.cElementTree as et
 
 
-def main():
-    tree = et.parse('./feed.xml')
+class Episode:
+    def __init__(self):
+        self.title = ""
+        self.link = ""
+
+
+def parse_feed(file):
+    tree = et.parse(file)
     root = tree.getroot()
+    episodes = []
 
     for item in root.iter('item'):
-        print('-------------------------')
+        episode = Episode()
+
         for child in item:
-            if child.text and "Schwurbel" in child.text:
-                print(child.text)
+            if child.tag == 'title':
+                episode.title = child.text
+
+            if child.tag == 'link':
+                episode.link = child.text
+
+            if child.tag == '{http://purl.org/rss/1.0/modules/content/}encoded':
+                if child.text and "Schwurbel" in child.text:
+                    # print(child.text)
+                    pass
+
+        episodes.append(episode)
+
+    return episodes
+
+
+def main():
+    episodes = parse_feed('./feed.xml')
+
+    for episode in episodes:
+        print(f"Title: {episode.title}")
+        print(f"Link: {episode.link}")
+        print('=========================')
 
 
 if __name__ == "__main__":
